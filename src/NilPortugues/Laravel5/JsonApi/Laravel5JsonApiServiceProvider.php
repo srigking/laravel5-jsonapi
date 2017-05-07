7d02c237c6116dbdc184c0acee901015f3148535
Sriram <srigmca13@gmail.com>
2017-05-07 22:47:23 +0530

    Added support for laravel 5.4

--- src/NilPortugues/Laravel5/JsonApi/Laravel5JsonApiServiceProvider.php
+++ src/NilPortugues/Laravel5/JsonApi/Laravel5JsonApiServiceProvider.php
@@ -14,6 +14,7 @@ namespace NilPortugues\Laravel5\JsonApi;
 use Illuminate\Support\ServiceProvider;
 use NilPortugues\Laravel5\JsonApi\Providers\Laravel51Provider;
 use NilPortugues\Laravel5\JsonApi\Providers\Laravel52Provider;
+use NilPortugues\Laravel5\JsonApi\Providers\Laravel54Provider;
 
 class Laravel5JsonApiServiceProvider extends ServiceProvider
 {
@@ -55,6 +56,9 @@ class Laravel5JsonApiServiceProvider extends ServiceProvider
                 break;
             case false !== strpos($version, '5.2.'):
                 $provider = new Laravel52Provider();
+                break;
+            case false !== strpos($version, '5.4.'):
+                $provider = new Laravel54Provider();
                 break;
             default:
                 throw new \RuntimeException(
